30f5f21f03a00f1a00ff565a7bb9ba3cc67b8f79
Van Vo <>
2021-05-19 22:49:15 +0700

    bring in concurrent stack to simulate message queue, safely update retweet count

--- Pages/TweetList/TweetList.razor.cs
+++ Pages/TweetList/TweetList.razor.cs
@@ -14,7 +14,7 @@ namespace TwitterCodingChallenge.Pages.List
     {
         [Inject]
         public IJSRuntime JS { get; set; }
-        [Inject] protected ITweetService ProjectService { get; set; }
+        [Inject] protected ITweetService TweetService { get; set; }
 
         private readonly ListGridType _listGridType = new ListGridType
         {
@@ -27,15 +27,69 @@ namespace TwitterCodingChallenge.Pages.List
             Xxl = 4
         };
 
-        
+        private List<TweetItem> _data = new List<TweetItem>();
+        bool submitting = false;
+        string _value = "";
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
-            var data = await ProjectService.GetTweetsListAsync(10);
-            _data = data;
+            _data = await TweetService.GetTweetsListAsync(10);
         }
 
-        
+        async void OnSubmit()
+        {
+            submitting = true;
+
+            await Task.Delay(1000);
+
+            var newTweet = new TweetItem()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Elon Musk",
+                ProfileImage = "https://pbs.twimg.com/profile_images/1383184766959120385/MM9DHPWC_400x400.jpg",
+                Username = "elonmusk",
+                Content = _value,
+                CreatedAt = DateTime.Now,
+            };
+
+            _data.Add(newTweet);
+            await TweetService.CreateNewTweet(newTweet);
+
+            submitting = false;
+            _value = "";
+            await InvokeAsync(StateHasChanged);
+        }
+
+        async void OnRetweetClick(TweetItem tweet)
+        {
+            submitting = true;
+            var tweetToLinkId = tweet.ParentTweetId != Guid.Empty ? tweet.ParentTweetId : tweet.Id;
+            var newTweet = new TweetItem()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Elon Musk",
+                ProfileImage = "https://pbs.twimg.com/profile_images/1383184766959120385/MM9DHPWC_400x400.jpg",
+                Username = "elonmusk",
+                Content = _value,
+                CreatedAt = DateTime.Now,
+                ParentTweetId = tweetToLinkId
+            };
+
+            //Persist reweet count into database using message queue
+            await TweetService.CreateNewTweet(newTweet);
+            await TweetService.UpdateReweetCount(tweetToLinkId);
+
+            // Relect UI changes
+            _data.Add(newTweet);
+            _data = _data.OrderByDescending(x => x.RetweetCount).ToList();
+            await InvokeAsync(StateHasChanged);
+            submitting = false;
+
+        }
+        async void OnCommentClick(TweetItem tweet)
+        {
+            await InvokeAsync(StateHasChanged);
+        }
     }
 }
--- Services/TweetService.cs
+++ Services/TweetService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -9,11 +11,15 @@ namespace TwitterCodingChallenge.Services
 {
     public interface ITweetService
     {
-        Task<List<TweetItem>> GetTweetsListAsync(int count = 0);
+        Task<List<TweetItem>> GetTweetsListAsync(int count = 10);
+        Task<List<TweetItem>> CreateNewTweet(TweetItem tweet);
+        Task<bool> UpdateReweetCount(Guid tweetId);
     }
 
     public class TweetService : ITweetService
     {
+        private ConcurrentStack<Guid> stack = new ConcurrentStack<Guid>();
+        private List<TweetItem> tweets = new List<TweetItem>();
         private readonly HttpClient _httpClient;
 
         public TweetService(HttpClient httpClient)
@@ -21,12 +27,34 @@ namespace TwitterCodingChallenge.Services
             _httpClient = httpClient;
         }
 
-        public async Task<List<TweetItem>> GetTweetsListAsync(int count = 0)
+        public async Task<List<TweetItem>> GetTweetsListAsync(int count = 10)
         {
             var data = await _httpClient.GetFromJsonAsync<TweetItem[]>("data/sample_tweets.json");
-            var result = new List<TweetItem>();
-            result.AddRange(data);
-            return count > 0 ? result.OrderByDescending(x => x.RetweetCount).Take(count).ToList(): result;
+            tweets.AddRange(data);
+            return count > 0 ? tweets.OrderByDescending(x => x.RetweetCount).Take(count).ToList(): tweets.ToList();
+        }
+
+        public async Task<List<TweetItem>> CreateNewTweet(TweetItem tweet)
+        {
+            tweets.Add(tweet);
+            return tweets.OrderByDescending(x => x.RetweetCount).ToList();
+        }
+
+        public async Task<bool> UpdateReweetCount(Guid tweetId)
+        {
+            stack.Push(tweetId);
+
+            if (!stack.IsEmpty)
+            {
+                Guid tweetToUpdateId;
+                stack.TryPop(out tweetToUpdateId);
+
+                //Simulate updating reweet count
+                tweets.FirstOrDefault(x => x.Id == tweetId).RetweetCount += 1;
+
+                return true;
+            }
+            return false;
         }
     }
 }
